da250d76f867573f041fef7ef0421ad0ef5c1645
Sushant Lama <sushantlama732@gmail.com>
2024-12-11 08:43:42 +0545

    add loading

--- src/Page/Login/Login.jsx
+++ src/Page/Login/Login.jsx
@@ -1,62 +1,88 @@
-import React, { useContext, useState } from 'react'
-import { AuthContext } from '../../UseContext/Auth'
-import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../../UseContext/Auth";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const { email, dispatch, error } = useContext(AuthContext);
+  const [loading, setLoading] = useState(false);
+  // console.log('email', email);
+  const [credential, setData] = useState({
+    email: undefined,
+    password: undefined,
+  });
 
-    const {email, dispatch, error}= useContext(AuthContext)
-    // console.log('email', email);
-    const[credential,setData]=useState({
-        email: undefined,
-        password: undefined
-    })
-   
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-      const handleChnage=(e)=>{
-        const {id, value}= e.target
-        setData({...credential, [id]: value})
-        // console.log(data);
-      }
-    //   console.log(data);
+  const handleChnage = (e) => {
+    const { id, value } = e.target;
+    setData({ ...credential, [id]: value });
+    // console.log(data);
+  };
 
-const handleClick =async(e)=>{
-    e.preventDefault()
-   dispatch({type:'Login_Start'})
-   try {
-    const res = await axios.post('https://naikap-node-api.onrender.com/user/login', credential)
-    console.log('Login_Start', res);
-    dispatch({type:'Login_Success', payload: res.data.details})
-    navigate("/")
-    
-   } 
-   catch (error) {
+  const handleClick = async (e) => {
+    e.preventDefault();
+    setLoading(true); // Start loading
+    dispatch({ type: "Login_Start" });
 
-    console.log(error);
-    
-     dispatch({ type: "Login_Fail", payload: error.response.data })
-     navigate("/login")
-   }
-      }
-    
+    try {
+      const res = await axios.post(
+        "https://naikap-node-api.onrender.com/user/login",
+        credential
+      );
+      console.log("Login_Start", res);
+      dispatch({ type: "Login_Success", payload: res.data.details });
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+      dispatch({ type: "Login_Fail", payload: error.response.data });
+      navigate("/login");
+    } finally {
+      setLoading(false); // Stop loading
+    }
+  };
+
   return (
-    <div className=' h-[100vh] flex justify-center items-center'>
-    <form className='flex flex-col gap-5 border border-gray-800 rounded-md px-6  py-4' onSubmit={handleClick}>
-            <label htmlFor="">Email</label>
-               <input  type="text" id="email" onChange={handleChnage}  className=' p-1 border-2 rounded-md'  />
-               <label htmlFor="">Password</label>
-               <input type="text" id="password" onChange={handleChnage}    className=' p-1 border-2 rounded-md'  />
+    <div className=" h-[100vh] flex justify-center items-center">
+      <form
+        className="flex flex-col gap-5 border border-gray-800 rounded-md px-6  py-4"
+        onSubmit={handleClick}
+      >
+        <label htmlFor="">Email</label>
+        <input
+          type="email"
+          id="email"
+          required
+          onChange={handleChnage}
+          className=" p-1 border-2 rounded-md"
+        />
+        <label htmlFor="">Password</label>
+        <input
+          type="text"
+          id="password"
+          required
+          onChange={handleChnage}
+          className=" p-1 border-2 rounded-md"
+        />
 
-          
-       
-{error  && <p className=' text-red-600'>{error}</p> }
-<button className=' bg-lime-400 p-2 border rounded-xl' type='submit'>Login</button>
-<Link to='/signup'>   <span className=' px-2 py-1 text-blue-600 rounded-lg'>Dont have account? Sign Up</span></Link>
-   
+        {error && <p className=" text-red-600">{error}</p>}
+        <button
+          className={`bg-lime-400 p-2 border rounded-xl ${
+            loading && "opacity-50 cursor-not-allowed"
+          }`}
+          type="submit"
+        >
+          {loading ? "Loading..." : "Login"}
+        </button>
+        <Link to="/signup">
+          {" "}
+          <span className=" px-2 py-1 text-blue-600 rounded-lg">
+            Dont have account? Sign Up
+          </span>
+        </Link>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/Page/SignUp/SignUp.jsx
+++ src/Page/SignUp/SignUp.jsx
@@ -1,67 +1,109 @@
-import axios from 'axios'
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
-    const [credential, setCredential]= useState({
-        email: undefined,
-        password: undefined,
-        userName: undefined,
-        images: null
-    })
+  const [credential, setCredential] = useState({
+    email: undefined,
+    password: undefined,
+    userName: undefined,
+    images: null,
+  });
+  const [loading, setLoading] = useState(false);
 
-    const handleChnage =( e)=>{
-  const{id, value}= e.target
-        setCredential({...credential, [id]: value})
+  const handleChnage = (e) => {
+    const { id, value } = e.target;
+    setCredential({ ...credential, [id]: value });
+  };
+  const handleImage = (e) => {
+    const file = e.target.files[0];
+    console.log(file);
+    setCredential({ ...credential, images: file });
+  };
+
+  const navigate = useNavigate();
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const formData = new FormData();
+    formData.append("images", credential.images);
+    formData.append("userName", credential.userName);
+    formData.append("email", credential.email);
+    formData.append("password", credential.password);
+
+    try {
+      const res = await axios.post(
+        "https://naikap-node-api.onrender.com/user",
+        formData
+      );
+      console.log(res);
+      navigate("/login");
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
-    const handleImage=(e)=>{
-        const file = e.target.files[0]
-        console.log(file);
-        setCredential({...credential, images: file})
-    }
-
-
-
-
-    const navigate = useNavigate()
-    //handleClick
-
-    const handleClick = async (e) => {
-        e.preventDefault();
-        const formData = new FormData()
-        formData.append('images', credential.images)
-        formData.append('userName', credential.userName)
-        formData.append('email', credential.email)
-        formData.append('password', credential.password)
-      
-        try {
-          const res = await axios.post('https://naikap-node-api.onrender.com/user', formData);
-          console.log(res);
-          navigate("/login")
-
-    
-        } catch (error) {
-          console.log(error);
-        }
-      };
+  };
   return (
-    <div className=' h-[100vh] flex justify-center items-center'>
-        <form className='flex flex-col gap-5 border border-gray-800 rounded-md px-6  py-4' onSubmit={handleClick}>
-    <label htmlFor="">UserName</label>
-    <input type="text"  id="userName" onChange={handleChnage}   className=' p-1 border-2 rounded-md'  />
-    <label htmlFor="">Email</label>
-    <input type="email" id="email" onChange={handleChnage}   className=' p-1 border-2 rounded-md' />
-    <label htmlFor="">Password</label>
-    <input type="text" id="password" onChange={handleChnage}    className=' p-1 border-2 rounded-md' />
-    <label htmlFor="">Image</label>
-    <input type="file"  accept='.jpeg, .png, .jpg' id="images" onChange={handleImage} required   className=' p-1 border-2 rounded-md'  />
+    <div className=" h-[100vh] flex justify-center items-center">
+      <form
+        className="flex flex-col gap-5 border border-gray-800 rounded-md px-6  py-4"
+        onSubmit={handleClick}
+      >
+        <label htmlFor="">UserName</label>
+        <input
+          type="text"
+          id="userName"
+          onChange={handleChnage}
+          required
+          className=" p-1 border-2 rounded-md"
+        />
+        <label htmlFor="">Email</label>
+        <input
+          type="email"
+          id="email"
+          onChange={handleChnage}
+          required
+          className=" p-1 border-2 rounded-md"
+        />
+        <label htmlFor="">Password</label>
+        <input
+          type="text"
+          id="password"
+          onChange={handleChnage}
+          required
+          className=" p-1 border-2 rounded-md"
+        />
+        <label htmlFor="">Image</label>
+        <input
+          type="file"
+          accept=".jpeg, .png, .jpg"
+          id="images"
+          onChange={handleImage}
+          required
+          className=" p-1 border-2 rounded-md"
+        />
 
-    <button className=' bg-lime-400 p-2 border rounded-xl' type='submit'>Signin</button>
-    <Link to='/login'>   <span className=' px-2 py-1 text-blue-400 rounded-lg'>Already have account?</span></Link>
+        <button
+          className={`bg-lime-400 p-2 border rounded-xl mt-2 ${
+            loading ? "opacity-50 cursor-not-allowed" : ""
+          }`}
+          type="submit"
+          disabled={loading} // Disable if loading or fields are empty
+        >
+          {loading ? "Submitting..." : "Register"}
+        </button>
+        <Link to="/login">
+          {" "}
+          <span className=" px-2 py-1 text-blue-400 rounded-lg">
+            Already have account?
+          </span>
+        </Link>
       </form>
     </div>
+  );
+};
 
-  )
-}
-
-export default SignUp
+export default SignUp;
