4462d9c221c4a4823dccffdaa323eafb58d1dbc6
Sushant Lama <sushantlama732@gmail.com>
2024-12-11 08:47:17 +0545

    added login on another

--- src/Page/Gallery/Gallery.jsx
+++ src/Page/Gallery/Gallery.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
-import Navbar from '../../Component/Navbar/Navbar'
-import Footer from '../../Component/Footer/Footer'
+import React from "react";
+import Navbar from "../../Component/Navbar/Navbar";
+import Footer from "../../Component/Footer/Footer";
 const Gallery = () => {
   return (
     <>
-    <Navbar/>
-    <div className=' min-h-[50vh]'>
-   <h3 className=' text-center font-serif text-3xl'> Its on work </h3>
-    </div>
-  
-   <Footer/>
-   </>
-  )
-}
+      <Navbar />
+      <div className="min-h-screen flex flex-col md:flex-row items-center justify-evenly">
+        <img
+          src="/Maintence.webp"
+          className="w-full md:max-w-[50%] max-h-[450px]"
+          alt="maintence"
+        />
+        <h1 className="text-center text-3xl text-green-85bc22 mt-4 md:mt-0">
+          Sorry for the inconvenience, it's on work
+        </h1>
+      </div>
 
-export default Gallery
+      <Footer />
+    </>
+  );
+};
+
+export default Gallery;
